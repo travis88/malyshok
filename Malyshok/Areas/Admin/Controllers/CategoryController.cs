b289d96d303f5b133ba391a143d47dd8f06716c1
travis88 <hellsballs99@gmail.com>
2018-02-07 18:19:39 +0300

    list of categories to admin

---
+++ Malyshok/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using Disly.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Disly.Areas.Admin.Controllers
+{
+    public class CategoryController : CoreController
+    {
+        private CategoryViewModel model;
+
+        /// <summary>
+        /// Обрабатывается до вызыва экшена
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            ViewBag.ControllerName = (String)RouteData.Values["controller"];
+            ViewBag.ActionName = RouteData.Values["action"].ToString().ToLower();
+
+            model = new CategoryViewModel
+            {
+                Account = AccountInfo,
+                Settings = SettingsInfo,
+                UserResolution = UserResolutionInfo,
+                ControllerName = ControllerName,
+                ActionName = ActionName
+            };
+
+            #region Метатеги
+            ViewBag.Title = UserResolutionInfo.Title;
+            ViewBag.Description = "";
+            ViewBag.KeyWords = "";
+            #endregion
+        }
+
+        // GET: Admin/Category
+        [HttpGet]
+        public ActionResult Index()
+        {
+            // наполняем модель списком категорий
+            model.List = _cmsRepository.getCategories();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Item(Guid id)
+        {
+            // единичная запись
+            model.Item = _cmsRepository.getCategory(id);
+            
+            // список дочерних эл-тов
+            model.List = _cmsRepository.getCategories(id);
+
+            return View(model);
+        }
+    }
+}
---
+++ Malyshok/Areas/Admin/Models/CategoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using cms.dbModel.entity;
+using System.Collections.Generic;
+
+namespace Disly.Areas.Admin.Models
+{
+    /// <summary>
+    /// Модель для представления категорий
+    /// </summary>
+    public class CategoryViewModel : CoreViewModel
+    {
+        /// <summary>
+        /// Единичная запись
+        /// </summary>
+        public CategoryModel Item { get; set; }
+
+        /// <summary>
+        /// Список категорий
+        /// </summary>
+        public IEnumerable<CategoryModel> List { get; set; }
+    }
+}

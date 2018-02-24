dada314ec93c2c9d1b75657b92f472865c18e2a4
travis88 <hellsballs99@gmail.com>
2018-02-24 18:36:52 +0300

    react добавлен в проект

---
+++ Malyshok/webpack.config.js
@@ -0,0 +1,30 @@
+﻿const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js',
+        sourceMapFilename: "bundle.map"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'stage-0', 'react']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            warnings: false,
+            mangle: true
+        })
+    ]
+}

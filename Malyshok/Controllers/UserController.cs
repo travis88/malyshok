c9546f62e5c08cbbb40a4806632dc4bf2ac4a097
DmitryBoriskin <dmitry@boriskiny.ru>
2018-03-02 20:24:17 +0300

    Регистрация

--- Malyshok/Controllers/UserController.cs
+++ Malyshok/Controllers/UserController.cs
@@ -1,7 +1,6 @@
-﻿using cms.dbase;
+﻿using cms.dbModel.entity;
 using Disly.Models;
 using System;
-using System.Linq;
 using System.Web.Mvc;
 
 namespace Disly.Controllers
@@ -21,7 +20,7 @@ namespace Disly.Controllers
             {
                 SitesInfo = siteModel,
                 SiteMapArray = siteMapArray,
-                Breadcrumbs= breadcrumb,
+                Breadcrumbs = breadcrumb,
                 BannerArray = bannerArray,
                 ProdCatalog = category_list,
                 CurrentPage = currentPage
@@ -35,7 +34,7 @@ namespace Disly.Controllers
         public ActionResult Login()
         {
             //string _ViewName = (ViewName != String.Empty) ? ViewName : "~/Views/Error/CustomError.cshtml";
-            
+
 
             return View(model);
         }
@@ -44,6 +43,55 @@ namespace Disly.Controllers
         {
             //string _ViewName = (ViewName != String.Empty) ? ViewName : "~/Views/Error/CustomError.cshtml";
 
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Reg(regModel backModel)
+        {
+            //ErrorMassege userMassege = new ErrorMassege();
+            //userMassege.title = "Информация";
+
+            if (ModelState.IsValid)
+            {
+                string PrivateKey = Settings.SecretKey;
+                string EncodedResponse = Request["g-Recaptcha-Response"];
+                bool IsCaptchaValid = (ReCaptchaClass.Validate(PrivateKey, EncodedResponse).ToLower() == "true" ? true : false);
+
+                if (IsCaptchaValid)
+                {
+                    char[] _pass = backModel.Pass.Password.ToCharArray();
+                    Cripto password = new Cripto(_pass);
+                    string NewSalt = password.Salt;
+                    string NewHash = password.Hash;
+
+                    UsersModel User = new UsersModel();
+                    User.Id = Guid.NewGuid();
+                    User.FIO = backModel.UserName;
+                    User.Phone = backModel.Phone;
+                    User.EMail = backModel.Mail;
+                    User.Address = backModel.Adress;
+                    User.Salt = NewSalt;
+                    User.Hash = NewHash;
+                    User.Disabled = true;
+
+                    ModelState.AddModelError("", "Всё супер.");
+                    //return RedirectToAction("SendMail", "Page");
+                }
+                else
+                {
+                    //userMassege.info = "Не пройдена проверка \"Я не робот\".";
+                    ModelState.AddModelError("", "Не пройдена проверка \"Я не робот\".");
+                }
+            }
+            else
+            {
+                //userMassege.info = "Произошла ошибка, попробуйте снова.";
+                ModelState.AddModelError("", "Произошла ошибка, попробуйте снова.");
+            }
+
+            //model.ErrorInfo = userMassege;
 
             return View(model);
         }
---
+++ Malyshok/Models/UserViewModel.cs
@@ -0,0 +1,35 @@
+﻿using cms.dbModel.entity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Disly.Models
+{
+    /// <summary>
+    /// Модель для страницы контактов
+    /// </summary>
+    public class regModel
+    {
+        public bool Type { get; set; }
+        public string OrgName { get; set; }
+
+        /// <summary>
+        /// Название
+        /// </summary>
+        [Required(ErrorMessage = "Поле «ФИО» не должно быть пустым.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Поле «Адрес» не должно быть пустым.")]
+        public string Adress { get; set; }
+
+        [Required(ErrorMessage = "Поле «Адрес» не должно быть пустым.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Поле «Адрес» не должно быть пустым.")]
+        public string Mail { get; set; }
+
+        public PasswordModel Pass { get; set; }
+
+        public bool Compliance { get; set; }
+    }
+
+}

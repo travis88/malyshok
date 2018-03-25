5b61168f7340ca65e565c8ad7dae746084153232
travis88 <hellsballs99@gmail.com>
2018-03-25 09:37:01 +0300

    рефакторинг импорта

---
+++ Import.Core/Helpers/InsertHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Import.Core.Helpers
+{
+    /// <summary>
+    /// Помощник для вставки
+    /// </summary>
+    public class InsertHelper
+    {
+        /// <summary>
+        /// Файловый поток
+        /// </summary>
+        public FileStream FileStream { get; set; }
+
+        /// <summary>
+        /// Контекст
+        /// </summary>
+        public dbModel Db { get; set; }
+
+        /// <summary>
+        /// Перечисляемая сущность
+        /// </summary>
+        public Entity Entity { get; set; }
+    }
+}
--- Import.Core/ServiceLogger.cs
+++ Import.Core/ServiceLogger.cs
@@ -11,34 +11,79 @@ namespace Import.Core
     /// </summary>
     public static class SrvcLogger
     {
-
+        /// <summary>
+        /// Логгер
+        /// </summary>
         private static Logger mLogger = LogManager.GetCurrentClassLogger();
 
+        /// <summary>
+        /// Дебаг
+        /// </summary>
+        /// <param name="perifix"></param>
+        /// <param name="message"></param>
+        /// <param name="exception"></param>
+        /// <param name="extParams"></param>
         public static void Debug(string perifix, string message, Exception exception = null, Dictionary<String, Object> extParams = null)
         {
             mLogger.Debug(MakeLogString(perifix, message, exception, extParams));
         }
 
+        /// <summary>
+        /// Инфо
+        /// </summary>
+        /// <param name="perifix"></param>
+        /// <param name="message"></param>
+        /// <param name="exception"></param>
+        /// <param name="extParams"></param>
         public static void Info(string perifix, String message, Exception exception = null, Dictionary<String, Object> extParams = null)
         {
             mLogger.Info(MakeLogString(perifix, message, exception, extParams));
         }
 
+        /// <summary>
+        /// Предупреждение
+        /// </summary>
+        /// <param name="perifix"></param>
+        /// <param name="message"></param>
+        /// <param name="exception"></param>
+        /// <param name="extParams"></param>
         public static void Warn(string perifix, String message, Exception exception = null, Dictionary<String, Object> extParams = null)
         {
             mLogger.Warn(MakeLogString(perifix, message, exception, extParams));
         }
 
+        /// <summary>
+        /// Ошибка
+        /// </summary>
+        /// <param name="perifix"></param>
+        /// <param name="message"></param>
+        /// <param name="exception"></param>
+        /// <param name="extParams"></param>
         public static void Error(string perifix, String message, Exception exception = null, Dictionary<String, Object> extParams = null)
         {
             mLogger.Error(MakeLogString(perifix, message, exception, extParams));
         }
 
+        /// <summary>
+        /// Критическая ошибка
+        /// </summary>
+        /// <param name="perifix"></param>
+        /// <param name="message"></param>
+        /// <param name="exception"></param>
+        /// <param name="extParams"></param>
         public static void Fatal(string perifix, String message, Exception exception = null, Dictionary<String, Object> extParams = null)
         {
             mLogger.Fatal(MakeLogString(perifix, message, exception, extParams));
         }
 
+        /// <summary>
+        /// Создание строки сообщения
+        /// </summary>
+        /// <param name="perifix"></param>
+        /// <param name="message"></param>
+        /// <param name="exception"></param>
+        /// <param name="extParams"></param>
+        /// <returns></returns>
         private static string MakeLogString(string perifix, string message, Exception exception = null, Dictionary<String, Object> extParams = null)
         {
             var extParamsString = "<none>";
